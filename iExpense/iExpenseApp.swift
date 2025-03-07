db8e67a1522954d6667ae94f1199c9ae41c232ac
Linh Nguyen <linh@linh-n.com>
2025-03-07 15:17:32 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Linh Nguyen on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
