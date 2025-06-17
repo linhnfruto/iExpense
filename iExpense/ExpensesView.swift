df7cdc5756a290777465612fdfb6994fcd4ecf74
Linh Nguyen <linh@linh-n.com>
2025-06-18 05:01:14 +0700

    Finish Day 59

---
+++ iExpense/ExpensesView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesView.swift
+//  iExpense
+//
+//  Created by Linh Nguyen on 18/6/25.
+//
+
+import SwiftUI
+
+struct ExpensesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesView()
+}
